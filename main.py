f43d906fbed5da3b58313be784fd46476db6a263
YaDanisimo <yako.daniil09@gmail.com>
2026-09-25 12:42:27 +0300

    project structure

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # здесь происходит управление всеми скриптами
+from game.board_generator import board_generate
+from game.menu import main_menu
+
+
+while True:
+    main_menu()
+    player_command = input("Введите команду").lower()
+    if player_command in ("1", "играть"):
+        print("game start")
+    elif player_command in ("2", "выход"):
+        break
+    else:
+        print("Введена неверная команда")
